9dabdd9eaba7bce723071e1b9abc02e457beea67
Firoz Khan <f90khan@gmail.com>
2016-04-17 21:42:03 +0530

    Initial Commit

---
+++ AnimationTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AnimationTest
+//
+//  Created by Santosh Narawade on 08/04/16.
+//  Copyright (c) 2016 Santosh Narawade. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AnimationTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AnimationTest
+//
+//  Created by Santosh Narawade on 08/04/16.
+//  Copyright (c) 2016 Santosh Narawade. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
